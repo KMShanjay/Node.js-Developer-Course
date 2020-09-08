153807c4c5ef69ce0db04ab5fa537263a5cd2426
KMShanjay <64511158+KMShanjay@users.noreply.github.com>
2020-09-08 14:41:29 +0530

    Update app.js

--- Section 2/app.js
+++ Section 2/app.js
@@ -3,3 +3,14 @@ const fs = require('fs')
 
 fs.writeFileSync('notes.txt', 'My name is Shanjay KM.')
 
+
+
+Challenge in Section I
+
+Append a data in fileStream
+
+const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'My name is Shanjay.')
+
+fs.appendFileSync('notes.txt', ' I live in Tamil Nadu.')
